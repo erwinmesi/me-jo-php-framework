857cca82292adccf0fa2710561e468b2f4ea5e58
Erwin Mesias <mesiaserwin@gmail.com>
2019-07-16 05:10:52 +0800

    Error 404 Page

---
+++ resources/views/errors/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Me-Jo PHP Framework</title>
+    </head>
+
+    <body>
+        <div class="container pt-5">
+            <h5 class="mb-0">Page Not Found.</h5>
+            <h1>Error 404</h1>
+            <a href="/">Back to Home page</a>
+        </div>
+    </body>
+</html>
